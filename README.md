b0185ab58986722aa85d23ad3476e1f4ae04e68e
Java <indead1124@gmail.com>
2021-04-09 10:19:28 -0400

    Move Files

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ### 📄 Todo Ideas
 
 - [ ] Randomize Mobs
+- [ ] Randomize Travels (maps)
 - [ ] Maps 3-11
 - [ ] More Items and Mobs
 - [ ] Player Upgrades
---
+++ assets/32x32.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="32x32" tilewidth="32" tileheight="32" tilecount="1056" columns="48">
+ <image source="32x32.jpg" width="1536" height="704"/>
+ <tile id="1" probability="0.05"/>
+ <tile id="2" probability="0.05"/>
+ <tile id="3" probability="0.05"/>
+ <tile id="4" probability="0.05"/>
+</tileset>
