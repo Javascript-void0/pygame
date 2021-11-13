ad98beb36314872f9327f784bf0e90cbb9ea4683
Java <indead1124@gmail.com>
2021-11-13 10:31:05 -0500

    part 8

--- tcod/death_functions.py
+++ tcod/death_functions.py
@@ -7,7 +7,7 @@ def kill_player(player):
     player.char = '%'
     player.color = libtcod.dark_red
 
-    return Message('You died!', libtcod.red, GameStates.PLAYER_DEAD)
+    return Message('You died!', libtcod.red), GameStates.PLAYER_DEAD
 
 def kill_monster(monster):
     death_message =  Message(f'{monster.name.capitalize()} is dead!', libtcod.orange)
