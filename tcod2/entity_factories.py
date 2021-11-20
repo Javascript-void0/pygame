678fea5c08102ab1a36b374dab95f7e926f25951
Java <indead1124@gmail.com>
2021-11-20 16:18:34 -0500

    newer version

---
+++ tcod2/entity_factories.py
@@ -0,0 +1,6 @@
+from entity import Entity
+
+player = Entity(char="@", color=(251, 245, 239), name="Player", blocks_movement=True)
+
+orc = Entity(char="o", color=(60, 60, 106), name="Orc", blocks_movement=True)
+troll = Entity(char="T", color=(60, 60, 106), name="Troll", blocks_movement=True)
